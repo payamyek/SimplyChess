7c8dcaa7ca3ff12a375c7ab1a0d4e6d93e5bddfc
Payam Yektamaram <payamyek@gmail.com>
2022-11-27 23:08:58 -0500

    feat: spring starter code

---
+++ simplychess/src/main/groovy/payamyektamaram/simplychess/SimplychessApplication.groovy
@@ -0,0 +1,13 @@
+package payamyektamaram.simplychess
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SimplychessApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SimplychessApplication, args)
+	}
+
+}
